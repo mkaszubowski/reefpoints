44b30119cf933e3e2890f8d35b0d6b9cf64bb7b7
Dan McClain <git@danmcclain.net>
2016-02-01 11:10:48 -0500

    Update deploy script for dockyard production site

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -2,24 +2,17 @@
 
 set -e
 
-git config --global user.email "socko@dockyard.com"
-git config --global user.name "sockothesock"
-
-
-# This specifies the user who is associated to the GH_TOKEN
-USER="sockothesock"
-
-# sending output to /dev/null to prevent GH_TOKEN leak on error
-git remote rm origin
-git remote add origin https://${USER}:${GHTOKEN}@github.com/dockyard/reefpoints.git &> /dev/null
-
 # Push build/posts.json to homeport.dockyard.com
 bundle exec rake build
 
 chmod 600 ./reefpoints_deploy
-ssh-keyscan -H homeport.dockyard.com >> ~/.ssh/known_hosts
 scp -i ./reefpoints_deploy build/posts.json temp_deploy@homeport.dockyard.com:reefpoints/posts.json
 scp -i ./reefpoints_deploy build/new_sitemap.xml temp_deploy@homeport.dockyard.com:reefpoints/sitemap.xml
 scp -i ./reefpoints_deploy build/new_atom.xml temp_deploy@homeport.dockyard.com:reefpoints/atom.xml
+
+scp -i ./reefpoints_deploy build/posts.json production@production.dockyard.com:uploads/posts.json
+scp -i ./reefpoints_deploy build/new_sitemap.xml production@production.dockyard.com:uploads/sitemap.xml
+scp -i ./reefpoints_deploy build/new_atom.xml production@production.dockyard.com:uploads/atom.xml
+ssh -i ./reefpoints_deploy production@production.dockyard.com ./load_posts.sh
 
 echo -e "Done\n"
